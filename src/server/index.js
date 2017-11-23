c1e8c69bd353f8ca99ed5dde6b115ce35e99a57b
Fernando Zabin <fernando.zabin@gmail.com>
2017-11-23 11:31:10 -0200

    Cria a estrutura do projeto

---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+
+const restify = require('restify')
+const server = restify.createServer()
+const routes = require('../http/routes')
+const cors = require('./cors')
+
+server.pre(cors.preflight)
+server.use(cors.actual)
+
+routes(server)
+
+module.exports = server
